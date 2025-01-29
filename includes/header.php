e5a6d5f4d3848d1d8c5825453f48a40844a0c919
Emmanuel Obeng <emmallob14@gmail.com>
2025-01-29 09:06:08 +0000

    Add the heatmap snippet

--- includes/header.php
+++ includes/header.php
@@ -58,7 +58,26 @@ $websiteName = "Equator Commodities";
     <link href="assets/css/module-css/team.css" rel="stylesheet">
     <link href="assets/css/module-css/news.css" rel="stylesheet">
     <link href="assets/css/module-css/education-details.css" rel="stylesheet">
-
+    <!-- heatmap.com snippet -->
+    <script>
+    (function() {      
+        var _heatmap_paq = window._heatmap_paq || [];
+        var heatUrl = window.heatUrl = "https://dashboard.heatmap.com/";
+        function heatLoader(url, item) {
+        if(typeof handleSinglePagedWebsite !== 'undefined' && item == 'prep') return true;
+        var s = document.createElement("script"); s.type = "text/javascript"; 
+        s.src = url; s.async = false; s.defer = true; document.head.appendChild(s);
+        }
+        heatLoader(heatUrl+"preprocessor.min.js?sid=2785", "prep");
+        setTimeout(function() {
+        if(typeof _heatmap_paq !== "object" || _heatmap_paq.length == 0) {     
+            _heatmap_paq.push(["setTrackerUrl", heatUrl+"heatmap.php"]);
+            heatLoader(heatUrl+"heatmap-light.min.js?sid=2785", "heat");
+        }
+        }, 1000);
+    })();
+    </script>
+    <!-- End heatmap.com snippet Code -->
 </head>
 
 
